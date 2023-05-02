dbac317f8b8786275387d605bbf6627ccc5d8afb
kokiben <bennassarkaoutar@gmail.com>
2023-05-02 16:28:42 -0700

    string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 
 /**
 * rev_string - Reverses a string
-* @s: The string to be modified 
+* @s: The string to be modified
 * Return: void
 */
 
@@ -20,4 +20,4 @@ tmp = s[index];
 s[index] = s[len - index - 1];
 s[len - index - 1] = tmp;
 }
-} 
+}
