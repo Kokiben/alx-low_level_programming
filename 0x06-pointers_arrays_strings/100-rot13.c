864d11a1acccfc4a6d21601c23a06f8611b320b6
kokiben <bennassarkaoutar@gmail.com>
2023-05-05 15:37:46 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * rot13 - encoder rot13
@@ -9,23 +8,41 @@
 */
 char *rot13(char *s)
 {
-int i;
-int j;
-char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+int index1 = 0, index2;
 
-for (i = 0; s[i] != '\0'; i++)
+char alphabet[52] = "'A', 'B', 'C', 'D', 'E', 'F',
+'G', 'H', 'I', 'J', 'K', 'L',
+'M', 'N', 'O', 'P', 'Q', 'R',
+'S', 'T', 'U', 'V', 'W', 'X',
+'Y', 'Z', 'a', 'b', 'c', 'd',
+'e', 'f', 'i', 'j', 'k', 'l',
+'m', 'n', 'o', 'p', 'q', 'r',
+'s', 't', 'u', 'v', 'w', 'x',
+'y', 'z'";
+char rot13key[52] = "'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+'U', 'V', 'W', 'X','Y','A',
+'B', 'C', 'D', 'E', 'F', 'G',
+'H', 'I', 'J', 'K', 'L', 'M',
+'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y',
+'z', 'a', 'b', 'c', 'd', 'e',
+'f', 'i', 'j', 'k', 'l','m'";
+while (str[index1])
+
+for (index2 = 0; index2 < 52; index2++)
 
 {
-for (i = 0; j < 52; j++)
+if (str[index1] == alphabet[index2])
+
 {
-if (s[i] == data1[j])
-{
-s[i] = datarot[j];
+str[index1] = rot13key[index2];
 break;
+
 }
 }
+index1++;
+
 }
-return (s);
+return (str);
 
 }
