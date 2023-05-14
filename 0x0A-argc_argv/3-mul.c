bacf65ea1e184c467dfd90b8438a5f60bdd4bcf3
kokiben <bennassarkaoutar@gmail.com>
2023-05-14 14:39:37 -0700

    args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,17 +11,22 @@
 */
 int main(int argc, char *argv[])
 {
+
 int n1 = 0,  n2 = 0;
+
 if (argc == 3)
 {
 n1 = atoi(argv[1]);
 n2 = atoi(argv[2]);
 printf(“%d\n”, n1 * n2);
+
 }
 Else
 {
+
 Printf(“Error\n”);
 return (1);
 }
+
 return (0);
 }
