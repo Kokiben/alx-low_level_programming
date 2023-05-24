a004cd7db27df20298e616e3399868ebe760f686
kokiben <bennassarkaoutar@gmail.com>
2023-05-23 18:23:11 -0700

    printdog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
 
 /**
 * print_dog - prints a dog
@@ -13,6 +13,6 @@ if (d)
 {
 printf("Name: %s\n", d->name ? d->name : "(nil)");
 printf("Age: %f\n", d->age);
-printf("owner: %s\n", d->owner ? d->owner : "(nil)");
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
 }
