0823cc9539d565f43bb6a6ce91d8156889e0c9c6
kokiben <bennassarkaoutar@gmail.com>
2023-05-24 19:15:46 -0700

    s

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
 * main - Prints the result of simple operation.
@@ -7,23 +9,24 @@
 *
 * Return: 0
 */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
-int (*op_func)(int, int), a, b;
+int (*oprt)(int, int);
 
 if (argc != 4)
-printf("Error\n"), exit(98);
+{
+printf("Error\n");
+exit(98);
+}
 
-a = atoi(argv[1]);
-b = atoi(argv[3];
+oprt = get_op_func(argv[2]);
 
-op_func = get_op_func(argv[2]);
-if (!op_func)
-printf("Error\n"), exit(99);
+if (!oprt)
+{
+printf("Error\n");
+exit(99);
+}
 
-if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-printf("Error\n"), exit(100);
-
-printf("%d\n", op_func(a, b));
+printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 return (0);
 }
