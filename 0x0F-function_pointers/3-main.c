86013c3559933b57cef67146dc8d5962d229c0b5
kokiben <bennassarkaoutar@gmail.com>
2023-05-26 16:04:03 -0700

    main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,32 +11,22 @@
 */
 int main(int argc, char *argv[])
 {
-int arg1, arg2, result;
+int (*oprt)(int, int);
 
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-arg1 = atoi(argv[1]);
-arg2 = atoi(argv[3]);
 
-func = get_op_func(argv[2]);
+oprt = get_op_func(argv[2]);
 
-if (!func)
+if (!oprt)
 {
 printf("Error\n");
 exit(99);
 }
-o = *argv[2];
-if ((0 == '/' || o == '%') && arg2 == 0)
-{
-printf("Error\n");
-exit(100);
-}
-result = func(arg1, arg2);
-
-printf("%d\n", result);
+printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
 
 return (0);
 }
