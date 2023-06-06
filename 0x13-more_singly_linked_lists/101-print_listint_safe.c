60decf8dc49ba519f20fdd296887946f2778d6cb
Kokiben <bennassarkaoutar@gmail.com>
2023-06-06 12:05:46 -0700

    freelist

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,22 +1,22 @@
 #include "lists.h"
 
 /**
-* free_listp - frees linked list
-* @head: head of list
+* free_listp - frees a linked list
+* @head: head of a list.
 *
-* Return: nop return nothind
+* Return: no return.
 */
 void free_listp(listp_t **head)
 {
 listp_t *temp;
-listp_t *bir;
+listp_t *curr;
 
 if (head != NULL)
 {
-bir = *head;
-while ((temp = bir) != NULL)
+curr = *head;
+while ((temp = curr) != NULL)
 {
-bir = bir->next;
+curr = curr->next;
 free(temp);
 }
 *head = NULL;
@@ -27,15 +27,14 @@ free(temp);
 * print_listint_safe - prints a linked list.
 * @head: head of a list.
 *
-* Return: number of the nodes in the list
+* Return: number of nodes in the list.
 */
-
 size_t print_listint_safe(const listint_t *head)
 {
-size_t numnode = 0;
-listp_t *hp, *new, *add;
+size_t nnodes = 0;
+listp_t *hptr, *new, *add;
 
-hp = NULL;
+hptr = NULL;
 while (head != NULL)
 {
 new = malloc(sizeof(listp_t));
@@ -44,27 +43,27 @@ if (new == NULL)
 exit(98);
 
 new->p = (void *)head;
-new->next = hp;
-hp = new;
+new->next = hptr;
+hptr = new;
 
-add = hp;
+add = hptr;
 
 while (add->next != NULL)
 {
 add = add->next;
-if (head == add->pre)
+if (head == add->p)
 {
-printf("-> [%p] %d\n", (void *)head, head->c);
-free_listp(&hp);
-return (numnode);
+printf("-> [%p] %d\n", (void *)head, head->n);
+free_listp(&hptr);
+return (nnodes);
 }
 }
 
-printf("[%p] %d\n", (void *)head, head->c);
+printf("[%p] %d\n", (void *)head, head->n);
 head = head->next;
-numnode++;
+nnodes++;
 }
 
-free_listp(&hp);
-return (numnode);
+free_listp(&hptr);
+return (nnodes);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,21 +2,21 @@
 
 /**
 * free_listp2 - frees a linked list
-* @head: pointer to the first node orhead of a list
+* @head: head of a list.
 *
-* Return: no return
+* Return: no return.
 */
 void free_listp2(listp_t **head)
 {
 listp_t *temp;
-listp_t *bir;
+listp_t *curr;
 
 if (head != NULL)
 {
-bir = *head;
-while ((temp = bir) != NULL)
+curr = *head;
+while ((temp = curr) != NULL)
 {
-bir = bir->next;
+curr = curr->next;
 free(temp);
 }
 *head = NULL;
@@ -31,22 +31,23 @@ free(temp);
 */
 size_t free_listint_safe(listint_t **h)
 {
-size_t numnode = 0;
-listp_t *hp, *new, *add;
-listint_t *bir;
+size_t nnodes = 0;
+listp_t *hptr, *new, *add;
+listint_t *curr;
 
-hp = NULL;
+hptr = NULL;
 while (*h != NULL)
 {
 new = malloc(sizeof(listp_t));
 
 if (new == NULL)
 exit(98);
-new->p = (void *)*h;
-new->next = hp;
-hp = new;
 
-add = hp;
+new->p = (void *)*h;
+new->next = hptr;
+hptr = new;
+
+add = hptr;
 
 while (add->next != NULL)
 {
@@ -54,17 +55,18 @@ add = add->next;
 if (*h == add->p)
 {
 *h = NULL;
-free_listp2(&hp);
-return (numnode);
+free_listp2(&hptr);
+return (nnodes);
 }
 }
 
-bir = *h;
+curr = *h;
 *h = (*h)->next;
-free(bir);
-numnode++;
+free(curr);
+nnodes++;
 }
+
 *h = NULL;
-free_listp2(&hp)
-return (numnode);
+free_listp2(&hptr);
+return (nnodes);
 }
