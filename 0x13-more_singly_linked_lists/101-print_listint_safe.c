b4c74d55f87192f980c78d328e1cbd61bae51b53
Kokiben <bennassarkaoutar@gmail.com>
2023-06-06 16:51:55 -0700

    print

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -13,14 +13,11 @@ size_t looped_listint_len(const listint_t *head)
 {
 const listint_t *tortoise, *hare;
 size_t nodes = 1;
-
 if (head == NULL || head->next == NULL)
 return (0);
-
 /* initialize the tortoise and hare pointers */
 tortoise = head->next;
 hare = (head->next)->next;
-
 /* iterate through the list until the hare pointer meets the tortoise pointer */
 while (hare)
 {
@@ -28,7 +25,6 @@ if (tortoise == hare)
 {
 /* if the pointers meet, reset the tortoise pointer to the head node */
 tortoise = head;
-
 /* iterate through the list with both pointers until they meet again */
 while (tortoise != hare)
 {
@@ -36,7 +32,6 @@ nodes++;
 tortoise = tortoise->next;
 hare = hare->next;
 }
-
 /* iterate through the rest of the loop to get the total number of nodes in the loop */
 tortoise = tortoise->next;
 while (tortoise != hare)
@@ -44,11 +39,9 @@ while (tortoise != hare)
 nodes++;
 tortoise = tortoise->next;
 }
-
 /* return the number of unique nodes in the loop */
 return (nodes);
 }
-
 /* update the tortoise and hare pointers */
 tortoise = tortoise->next;
 hare = (hare->next)->next;
@@ -67,10 +60,8 @@ return (0);
 size_t print_listint_safe(const listint_t *head)
 {
 size_t nodes, index = 0;
-
 /* check if the list is looped and get the number of unique nodes */
 nodes = looped_listint_len(head);
-
 /* if the list is not looped, print each node and increment the node count */
 if (nodes == 0)
 {
@@ -80,7 +71,6 @@ printf("[%p] %d\n", (void *)head, head->n);
 head = head->next;
 }
 }
-
 /* if the list is looped, print each node up to the loop point and the loop point itself */
 else
 {
@@ -89,10 +79,8 @@ for (index = 0; index < nodes; index++)
 printf("[%p] %d\n", (void *)head, head->n);
 head = head->next;
 }
-
 printf("-> [%p] %d\n", (void *)head, head->n);
 }
-
 /* return the number of nodes in the list */
 return (nodes);
 }
