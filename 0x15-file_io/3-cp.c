29e33839af2312ce4711144e2cbfbddf3a8b24ef
Kokiben <bennassarkaoutar@gmail.com>
2023-06-12 13:15:58 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,71 +2,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 1024
-#define PERMISSIONS 0664
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
-* print_error - Prints an error message to standard error and exits the program
-*
-* @message: The error message to print
-* @filename: The name of the file to use in the error message, or NULL
-* @exit_code: The exit code to use when exiting the program
-*
-* Description: This function prints the given error message to standard error,
-* optionally including the given filename in the message. It then exits the
-* program with the given exit code.
+* create_buffer - to Allocates 1024 bytes for buffer
+* @file: The name of the file buffer is storing chars for
+* Return: pointer to the newly-allocated buffer
 */
-void print_error(char *message, char *filename, int exit_code)
-{
-if (filename == NULL)
-dprintf(STDERR_FILENO, "%s\n", message);
-else
-dprintf(STDERR_FILENO, "%s %s\n", message, filename);
 
-exit(exit_code);
+char *create_buffer(char *file)
+{
+char *buffer;
+
+buffer = malloc(sizeof(char) * 1024);
+
+if (buffer == NULL)
+{
+dprintf(STDERR_FILENO,
+"Error: Can't write to %s\n", file);
+exit(99);
+}
+
+return (buffer);
 }
 
 /**
-* main - Copies the content of one file to another
+* close_file - Closes file descriptors
+* @fd: The file descriptor to be closed
+* closed.
+*/
+void close_file(int fd)
+{
+int cl;
+
+cl = close(fd);
+
+if (cl == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+exit(100);
+}
+}
+
+/**
+* main - Copies the contents of a file to another file.
+* @argc: the number of arguments passed to the program
+* @argv: the array of arguments passed to the program
+* Return: 0 if successful, 97-100 on failure
 *
-* @argc: The number of arguments passed to the program
-* @argv: An array of strings containing the arguments
-*
-* Description: This program copies the content of one file to another.
+* Description: If the argument count is incorrect - return 97
+* If file_from does not exist or cannot be read - return 98
+* If file_to cannot be created or written to - return 99
+* If file_to or file_from cannot be closed - return 100
 */
 int main(int argc, char *argv[])
 {
-int fd_from, fd_to, read_count, write_count;
-char buffer[BUFFER_SIZE];
+int from, to, bytes_r, bytes_w;
+char *buffer;
 
+/* Check for the correct number of arguments */
 if (argc != 3)
-print_error("Usage: cp file_from file_to", NULL, 97);
-
-fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1)
-print_error("Error: Can't read from file", argv[1], 98);
-
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-if (fd_to == -1)
-print_error("Error: Can't write to", argv[2], 99);
-
-while ((read_count = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 {
-write_count = write(fd_to, buffer, read_count);
-if (write_count == -1)
-print_error("Error: Can't write to", argv[2], 99);
-if (write_count != read_count)
-print_error("Error: Short write to", argv[2], 99);
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
 }
 
-if (read_count == -1)
-print_error("Error: Can't read from file", argv[1], 98);
+buffer = create_buffer(argv[2]);
+from = open(argv[1], O_RDONLY);
+bytes_r = read(from, buffer, 1024);
+to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-if (close(fd_from) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+do {
+if (from == -1 || bytes_r == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+free(buffer);
+exit(98);
+}
 
-if (close(fd_to) == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+bytes_w = write(to, buffer, bytes_r);
+if (to == -1 || bytes_w == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+free(buffer);
+exit(99);
+}
+
+bytes_r = read(from, buffer, 1024);
+to = open(argv[2], O_WRONLY | O_APPEND);
+
+} while (bytes_r > 0);
+
+free(buffer);
+close_file(from);
+close_file(to);
 
 return (0);
 }
