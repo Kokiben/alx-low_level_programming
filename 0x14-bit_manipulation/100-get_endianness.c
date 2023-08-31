bfa59e433941492b634473340e2e18c98a9137ea
Kokiben <bennassarkaoutar@gmail.com>
2023-08-31 10:27:29 -0700

    get

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -2,20 +2,20 @@
 
 /**
 * get_endianness - Checks endianness.
-* Return: 0 If big endian, 1 if little endian.
+* Return: 0 if big endian, 1 if little endian.
 */
 int get_endianness(void)
 {
-int n, k;
+int i, k;
 char *s;
 
 /**
-* Value of n = 1 if big endian
+* Value of i = 1 if big endian
 * |00000000|00000000|00000000|00000001
-* So the s will take the first 8 bits which have value of 0
+* so the s will take the first 8 bits which have value of 0
 */
-n = 1;
-s = (char *)&n;
+i = 1;
+s = (char *)&i;
 k = *s + '0';
 return (k);
 }
