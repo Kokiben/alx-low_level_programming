ff6442f758324b0de4dbc0d58750307fbc504911
Kokiben <bennassarkaoutar@gmail.com>
2023-11-27 15:18:25 -0800

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,27 +1,47 @@
 #include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
  * read_textfile - reads and prints a text file.
- * @filename: The name of file to read.
+ * @filename: name of file to read.
  * @letters: num of letters it should read.
  *
- * Return: num of letters read and printed.
+ * Return: num of letters read/printed.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char buf[READ_BUF_SIZE *8];
-	ssize_t byte;
+ssize_t byte_wrt,byte_rd;
+char *buf;
+int fd;
 
-if (!filename || !letters)
-		return (0);
+if (filename == NULL)
+return (0);
 
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
-byte = read(fd, &buf[0], letters);
-byte = write(SDTOUT_FILENO, letters);
+buf = malloc(sizeof(char) * letters);
 
+if (buf == NULL)
+{
 close(fd);
-return (byte);
+return (0);
+}
+byte_rd = read(fd, buf, letters);
+
+if (byte_rd == -1)
+{
+free(buf);
+close(fd);
+return (0);
+}
+byte_wrt = write(STDOUT_FILENO, buf, byte_rd);
+
+free(buf);
+close(fd);
+if (byte_wrt == -1 || byte_wrt != byte_rd)
+return (0);
+return (byte_wrt);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,7 +6,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define READ_BUF_SIZE 1024
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int append_text_to_file(const char *filename, char *text_content);
 int create_file(const char *filename, char *text_content);
