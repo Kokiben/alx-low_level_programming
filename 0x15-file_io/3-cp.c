82fff63a19ea9263f25632e13210d428bc602847
Kokiben <bennassarkaoutar@gmail.com>
2023-11-27 16:38:37 -0800

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,21 +8,21 @@
 #define BUFFER_SIZE 1024
 
 /**
- * copie_usage_exit - the number of argument is not the correct one,
+ * copie_usage_THE_exit - the number of argument is not the correct one,
  * exit with code 97.
  */
-void copie_usage_exit(void)
+void copie_usage_THE_exit(void)
 {
 dprintf(2, "Usage: cp file_from file_to\n");
 exit(97);
 }
 
 /**
- * copie_error_exit - error message.
+ * copie_error_THE_exit - error message.
  * @messag: error message to print.
  * @exit_cod: The exit code to use.
  */
-void copie_error_exit(const char *messag, int exit_cod)
+void copie_error_THE_exit(const char *messag, int exit_cod)
 {
 dprintf(2, "%s\n", messag);
 exit(exit_cod);
@@ -30,12 +30,12 @@ exit(exit_cod);
 
 
 /**
- * close_file_exit - close file descriptors and exit .
+ * close_file_THE_exit - close file descriptors and exit .
  * @fd_from: file descriptor.
  * @fd_to: destination file descriptor.
  * @exit_code: exit code to use.
  */
-void close_file_exit(int fd_from, int fd_to, int exit_code)
+void close_file_THE_exit(int fd_from, int fd_to, int exit_code)
 {
 	close(fd_from);
 	close(fd_to);
@@ -49,28 +49,28 @@ int main(int argc, char *argv[])
 	char buf[BUFFER_SIZE];
 
 if (argc != 3)
-copie_usage_exit();
+copie_usage_THE_exit();
 
 fd_from = open(argv[1], O_RDONLY);
 if (fd_from == -1)
-copie_error_exit("Error: Can't read from file", 98);
+copie_error_THE_exit("Error: Can't read from file", 98);
 
 fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (fd_to == -1)
-copie_error_exit("Error: Can't write to file", 99);
+copie_error_THE_exit("Error: Can't write to file", 99);
 
 while ((byte_rd = read(fd_from, buf, BUFFER_SIZE)) > 0)
 {
 byte_wrt = write(fd_to, buf, byte_rd);
 if (byte_wrt == -1)
-close_file_exit(fd_from, fd_to, 99);
+close_file_THE_exit(fd_from, fd_to, 99);
 }
 
 if (byte_rd == -1)
-close_file_exit(fd_from, fd_to, 98);
+close_file_THE_exit(fd_from, fd_to, 98);
 
 if (close(fd_from) == -1 || close(fd_to) == -1)
-copie_error_exit("Error: Can't close fd", 100);
+copie_error_THE_exit("Error: Can't close fd", 100);
 
 chmod(argv[2], S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 
