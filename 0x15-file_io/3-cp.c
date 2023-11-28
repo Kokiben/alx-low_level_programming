63037aad6810acf202fa53869d1d49ee8ca6fcbf
Kokiben <bennassarkaoutar@gmail.com>
2023-11-27 16:58:59 -0800

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,85 +1,84 @@
 #include "main.h"
 #include <stdio.h>
 
-int file_closer(int);
+int fil_close(int);
 /**
- * main - Main function to copy files
- * @argc: The number of passed arguments
- * @argv: The pointers to array arguments
+ * main - function copy file
+ * @argc: num of arguments
+ * @argv: pointer arguments
  * Return: 1 on success, exits on failure
  */
 int main(int argc, char *argv[])
 {
-	char buffer[1024];
-	int bytes_read = 0, EndFile = 1, form = -1, fd = -1, error = 0;
+char buf[1024];
+int byte_rd = 0, FILEEND = 1, form = -1, fd = -1, err = 0;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
 
-	form = open(argv[1], O_RDONLY);
-	if (form < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+form = open(argv[1], O_RDONLY);
+if (form < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
 
-	fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-	if (fd < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		file_closer(form);
-		exit(99);
-	}
+fd = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+if (fd < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+fil_close(form);
+exit(99);
+}
 
-	while (EndFile)
-	{
-		EndFile = read(form, buffer, 1024);
-		if (EndFile < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			file_closer(form);
-			file_closer(fd);
-			exit(98);
-		}
-		else if (EndFile == 0)
-			break;
-		bytes_read += EndFile;
-		error = write(fd, buffer, EndFile);
-		if (error < 0)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			file_closer(form);
-			file_closer(fd);
-			exit(99);
-		}
-	}
-	error = file_closer(fd);
-	if (error < 0)
-	{
-		file_closer(form);
-		exit(100);
-	}
-	error = file_closer(form);
-	if (error < 0)
-		exit(100);
-	return (0);
+while (FILEEND)
+{
+FILEEND = read(form, buf, 1024);
+if (FILEEND < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+fil_close(form);
+fil_close(fd);
+exit(98);
+}
+else if (FILEEND == 0)
+break;
+byte_rd += EndFile;
+err = write(fd, buf, FILEEND);
+if (err < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+fil_close(form);
+fil_close(fd);
+exit(99);
+}
+}
+err = fil_close(fd);
+if (err < 0)
+{
+fil_close(form);
+exit(100);
+}
+err = fil_close(form);
+if (err < 0)
+exit(100);
+return (0);
 }
 
 /**
- * file_closer - A function that closes a file and prints
- * error when closed file
- * @Error_desc: Error_desc for closed file
+ * fil_close - close a file/prints, error when closed file
+ * @Err_dptor: close file
  * Return: 1 on success, -1 on failure
  */
-int file_closer(int Error_desc)
+int fil_close(int Err_dptor)
 {
-	int error;
+int err;
 
-	error = close(Error_desc);
-	if (error < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", Error_desc);
-	return (error);
+err = close(Err_dptor);
+if (error < 0)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", Err_dptor);
+return (err);
 }
