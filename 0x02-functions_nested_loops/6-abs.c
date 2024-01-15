8ff020f9853a18f8e7fdb4cb46cb2165602b6105
Kokiben <bennassarkaoutar@gmail.com>
2024-01-15 10:20:30 -0800

    main

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - ddzq
+ * @n: Description of parameter x
+ *
+ * Description: ddzq
+ * section header: Section description
+ * Return: Description of value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -(n);
+	}
+	return (n);
+}
